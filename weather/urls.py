fa1409f7142743ef6dfe63350828db25b9b28fac
JuniorPythonProgrammer <zorofly446@gmail.com>
2022-02-05 13:31:28 +0200

    frontend refinement

--- weather/urls.py
+++ weather/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('weather/', home),
+    path('weather/', home, name='home_url'),
     path('weather/now/', search_weather_now, name='search_weather_now_url'),
     path('weather/now/<str:city>', radio_button_weather_now, name='radio_button_weather_now_url'),
     path('weather/eightday/<str:city>', weather_8_day, name='weather_8_day_url'),
