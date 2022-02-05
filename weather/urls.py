92881ae824a4e95d51feef12b6d5e6281a68bba3
JuniorPythonProgrammer <zorofly446@gmail.com>
2022-02-05 10:56:45 +0200

    realization of radio button

--- weather/urls.py
+++ weather/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home),
-    path('weather/', weather_now, name='weather_now_url'),
-    path('weather8/<str:city>', weather_8_day, name='weather_8_day_url'),
+    path('weather/', home),
+    path('weather/now/', search_weather_now, name='search_weather_now_url'),
+    path('weather/now/<str:city>', radio_button_weather_now, name='radio_button_weather_now_url'),
+    path('weather/eightday/<str:city>', weather_8_day, name='weather_8_day_url'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -7,33 +7,43 @@ def home(request):
 
     return render(request, 'weather/home.html')
 
-def weather_now(request):
+def weather_now(city):
+    r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric&lang=ru")
+    data = r.json()
+
+    weather = {
+        'city': data["name"],
+        'cur_temp': round(data["main"]["temp"]),
+        'humidity': data["main"]["humidity"],
+        'pressure': data["main"]["pressure"],
+        'wind_speed': round(data["wind"]["speed"]),
+        'description_weather': data["weather"][0]["description"],
+        'weathen_icon': data["weather"][0]["icon"],
+        'precipitation': 0,
+    }
+    if 'rain' in data and 'snow' in data:
+        weather["precipitation"] = round(data["rain"]["1h"] + data["snow"]["1h"], 1)
+    elif 'rain' in data:
+        weather["precipitation"] = round(data["rain"]["1h"], 1)
+    elif 'snow' in data:
+        weather["precipitation"] = round(data["snow"]["1h"], 1)
+    else:
+        weather["precipitation"] = 0
+    
+    return weather
+
+def search_weather_now(request):
     city = request.GET.get('search', '')
 
     if city:
-        r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric&lang=ru")
-        data = r.json()
-
-        context = {
-            'city': data["name"],
-            'cur_temp': round(data["main"]["temp"]),
-            'humidity': data["main"]["humidity"],
-            'pressure': data["main"]["pressure"],
-            'wind_speed': round(data["wind"]["speed"]),
-            'description_weather': data["weather"][0]["description"],
-            'weathen_icon': data["weather"][0]["icon"],
-            'precipitation': 0,
-        }
-        if 'rain' in data and 'snow' in data:
-            context["precipitation"] = round(data["rain"]["1h"] + data["snow"]["1h"], 1)
-        elif 'rain' in data:
-            context["precipitation"] = round(data["rain"]["1h"], 1)
-        elif 'snow' in data:
-            context["precipitation"] = round(data["snow"]["1h"], 1)
-        else:
-            context["precipitation"] = 0
+        context = weather_now(city)
     else:
         pass
+    return render(request, 'weather/weather_now.html', context)
+
+def radio_button_weather_now(request, city):
+    context = weather_now(city)
+
     return render(request, 'weather/weather_now.html', context)
 
 def weather_8_day(request, city):
